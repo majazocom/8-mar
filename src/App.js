6a12c50bbf63e4695d1c2a716c4988065cea5681
majazocom <maja.gedda@zocom.se>
2021-03-08 09:45:28 +0100

    import of cool women

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let rolemodels = require('./assets/wit.json');
+  console.log(rolemodels);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>International Womens Day</h1>
       </header>
     </div>
   );
